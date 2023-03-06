4af3873103fca850f7afbec623fbd7b4311f91c5
jsta <stachel2@msu.edu>
2023-03-06 09:41:28 -0700

    georeferencing seems working, #1

---
+++ pyffp/utils.py
@@ -0,0 +1,17 @@
+import geopy
+import geojsoncontour
+import geopandas as gpd
+from geopy.distance import geodesic
+
+
+# convert x_2d to lon_range, y_2d to lat_range
+def get_dd(x, origin_lon=0, origin_lat=0):
+    return geodesic(kilometers=x * 0.001).destination(
+        geopy.Point(origin_lat, origin_lon), 0
+    )[0]
+
+
+def contour_to_gpkg(cp, out_file="test.gpkg"):
+    geojson = geojsoncontour.contour_to_geojson(contour=cp, ndigits=6, unit="m")
+    gdf = gpd.read_file(geojson, driver="GeoJSON")
+    gdf.to_file(out_file, driver="GPKG")
